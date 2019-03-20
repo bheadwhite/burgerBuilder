a2976308261872f98b926adf8843003a2885a68a
Brent Whitehead <brent.whitehead@skywest.com>
2019-03-20 07:28:13 -0600

    added a drawer toggle, centered mobile layout, changed order summary into a class to checkout lifecycles, restructured aux and layout components locations

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,22 +1,31 @@
 import React, {Component} from 'react';
 
-import Aux from '../../hoc/Aux'
+import Aux from '../../hoc/Aux/Aux'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 class Layout extends Component {
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerClosedHandler = () => {
         this.setState({showSideDrawer: false})
     }
+
+    sideDrawertoggleHandler = () => {
+        // this.setState({showSideDrawer: !this.state.showSideDrawer})
+        //dont use state in set state directly.
+        this.setState( (prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        } )
+    }
+
     render () {
         return (
             <Aux>
-                <Toolbar />
+                <Toolbar drawerToggleClicked={this.sideDrawertoggleHandler}/>
                 <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
                 <main className={classes.Content}>
                     {this.props.children}
